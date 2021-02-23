f2cb00cb0261d5ca046e00351107edc1288019ed
kawada <vgnsl94@gmail.com>
2021-02-24 00:39:18 +0900

    layout変更

---
+++ layouts/single.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <span>single layout</span>
+    <AppNavigation />
+    <hr>
+    <nuxt />
+    <nuxt-link to="/">トップへ戻る</nuxt-link>
+    <footer>
+      footer
+    </footer>
+  </div>
+</template>
+
+<script>
+import AppNavigation from '~/components/AppNavigation.vue'
+export default {
+  components: {
+    AppNavigation
+  }
+}
+</script>
+
+<style>
+
+</style>
