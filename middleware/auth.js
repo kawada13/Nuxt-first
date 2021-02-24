62f679bc363445e897f1fd1e1c8d24017379e002
kawada <vgnsl94@gmail.com>
2021-02-24 23:06:57 +0900

    middleware実装

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,20 @@
-export default () => {
-  if(process.client) {
-    console.log('browser');
-  } else {
-    console.log('SSR');
+import Cookie from 'universal-cookie'
+
+
+export default ({ req, route, redirect }) => {
+  console.log(route.path)
+  console.log(req)
+  if(['/'].includes(route.path)) {
+    return
   }
+  const cookies = req ? new Cookie(req.headers.cookie) : new Cookie
+  const credential = cookies.get('credential')
+
+  if(credential && route.path === '/login') {
+    return redirect('/')
+  }
+  if(!credential && route.path !== '/login') {
+    return redirect('/login')
+  }
+
 }
